09102ae3307ae395032e7b4f5acb9a63d319acec
stryder03 <stryder03@users.noreply.github.com>
2019-02-20 22:09:36 -0700

    added csv files

--- exp5/exp5_2n/mpi_ch_size.py
+++ exp5/exp5_2n/mpi_ch_size.py
@@ -1,12 +1,12 @@
 from mpi4py import MPI
 import time
-from sys import stdout
+import subprocess
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 host = MPI.Get_processor_name()
 payload_size = 32
-loss = .02
+loss = 0
 size = comm.Get_size()
 
 
@@ -16,25 +16,27 @@ with open(file_name, 'w') as csv:
     ColumnRow = 'rank, msg_num, loss%, timestamp\n'
     csv.write(ColumnRow)
 
-    while payload_size <= 2:
+    while loss <= 2.0:
         msg_num = 0
+        if rank == 0:
+            loss += 0.02
+            subprocess.run(['./tc_setup.sh', str(loss)])
+            print("At %d loss", loss)
         while msg_num < 100:
             if rank == 1:
                 data = bytes(payload_size)
                 time_send = time.time()
                 comm.send(data, dest=0, tag=msg_num)
-                csv.write('%d, %d %s, %f' % (rank, msg_num, payload_size, time_send))
+                csv.write('%d, %d %s, %f' % (rank, msg_num, loss, time_send))
                 csv.write('\n')
                 msg_num += 1
             elif rank == 0:
-                stdout.write('*')
                 data = comm.recv(source=1, tag=msg_num)
                 if data is not None:
                     time_recv = time.time()
-                    csv.write('%d, %d, %s, %f' % (rank, msg_num, payload_size, time_recv))
+                    csv.write('%d, %d, %s, %f' % (rank, msg_num, loss, time_recv))
                     csv.write('\n')
                     msg_num += 1
 
-        loss += 0.2
 
 

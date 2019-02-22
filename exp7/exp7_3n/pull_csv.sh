ec082049edd2fddc4a46fb4dd3543184fc6d200a
stryder03 <stryder03@users.noreply.github.com>
2019-02-21 21:12:45 -0700

    exp 8 complete

--- exp7/exp7_3n/pull_csv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-scp mpi3:results.csv ./mpi3_recv.csv
-scp mpi1:results.csv ./mpi1_send.csv
-scp mpi2:results.csv ./mpi2_send.csv
--- exp8/exp8_3n/mpi_ch_size.py
+++ exp8/exp8_3n/mpi_ch_size.py
@@ -5,7 +5,7 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 host = MPI.Get_processor_name()
 payload_size = 32
-loss = 0.2
+loss = 2
 size = comm.Get_size()
 
 
@@ -16,15 +16,14 @@ command = './tc_setup.sh ' + str(loss_per)
 arg = command.split()
 subprocess.run(arg)
 while msg_num < 100:
-    if rank != 0:
+    if rank == 0:
         data = bytes(payload_size)
-        comm.send(data, dest=0, tag=msg_num)
+        comm.send(data, dest=1, tag=msg_num)
+        comm.send(data, dest=2, tag=msg_num)
         msg_num += 1
-    elif rank == 0:
-        data = comm.recv(source=1, tag=msg_num)
+    elif rank != 0:
+        data = comm.recv(source=0, tag=msg_num)
         if data is not None:
             msg_num += 1
 
-
 print('ended')
-
